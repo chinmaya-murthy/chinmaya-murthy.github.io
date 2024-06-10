ac9b7cfc31a54219725a01c54e9c8954d2d21db5
chinmaya-murthy <75656840+chinmaya-murthy@users.noreply.github.com>
2024-06-10 14:11:54 +0530

    h

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as HashRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+  );
+}
+
+export default App;
